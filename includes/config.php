a66373aea144d7702b38cbba0a4b7054f8e1893f
Palys99 <77256621+Palys99@users.noreply.github.com>
2023-10-12 20:26:34 -0600

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','https://databases.000webhost.com/');
+define('DB_USER','id21392890_tt');
+define('DB_PASS' ,'{#]+6C88eN"l');
 define('DB_NAME', 'tt');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
